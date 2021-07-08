f1ea67fa6cabdc9a4cbf29524a882b8c02a3c88c
prabinshrestha323 <51945844+prabinshrestha323@users.noreply.github.com>
2021-07-08 22:07:32 +0545

    naya jos

---
+++ app.js
@@ -0,0 +1,55 @@
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser')
+var exphbs = require('express-handlebars');
+const session = require("express-session");
+const path = require("path")
+const app = express();
+
+mongoose.connect('mongodb://localhost/my_database', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+});
+mongoose.connection.on("connected", connected => {
+    console.log("connection to database....")
+})
+mongoose.connection.on("err", error => {
+    console.log("err")
+
+})
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
+////Express static folder
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+    session({
+        secret: "secret",
+        resave: true,
+        saveUninitialized: true
+    })
+);
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.get("/admin", (req, res) => {
+    res.render("admin")
+})
+//routes call
+const aboutrouter = require("./routes/about")
+app.use("/", aboutrouter)
+
+app.listen(2000, () => {
+    console.log("localhost://2000")
+})
---
+++ models/about.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+
+    about: {
+        type: String
+    }
+})
+
+module.exports = mongoose.model("about", (userSchema))
